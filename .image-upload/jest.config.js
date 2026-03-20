aa212c88219d178127c2e194f6e4bc032db3109f
Harry-Milesight <hshstar@qq.com>
2026-03-20 09:52:37 +0800

    chore: add Jest coverage threshold and reporters

--- .image-upload/jest.config.js
+++ .image-upload/jest.config.js
@@ -2,5 +2,14 @@ module.exports = {
   testEnvironment: 'node',
   testMatch: ['**/test/**/*.test.js'],
   collectCoverageFrom: ['lib/**/*.js'],
+  coverageThreshold: {
+    global: {
+      branches: 80,
+      functions: 80,
+      lines: 80,
+      statements: 80
+    }
+  },
+  coverageReporters: ['text', 'text-summary', 'html'],
   verbose: true
 };
